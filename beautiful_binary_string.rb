6003ba7d349d0376d82c02b62e6a795ead80693c
tlantz77 <tlantz@gmail.com>
2016-09-27 16:00:07 -0500

    change beautiful binary solution

--- beautiful_binary_string.rb
+++ beautiful_binary_string.rb
@@ -1,13 +1,4 @@
 n = gets.strip.to_i
 B = gets.strip
 
-steps = 0
-
-for i in 1..B.length - 2
-    if B[i-1].to_s + B[i].to_s + B[i+1].to_s == "010"
-        B[i+1] = "1"
-        steps += 1
-    end
-end
-
-puts steps
+puts B.scan("010").length
